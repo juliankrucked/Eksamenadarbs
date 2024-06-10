cb9c53b1db822bbaa2541ee2e82595d9cbd14370
lauris <solaimans1203@gmail.com>
2024-06-10 19:53:21 +0300

    jautajumu ielade un pārbaude

--- src/tests.java
+++ src/tests.java
@@ -1,12 +1,15 @@
 import java.io.*;
+import javax.swing.*;
 import java.util.*;
+
+
 class Jautajumi{
-	String jautajumi;
+	String jautajums;
 	List<String> atbildes;
 	char pareizAtb;
 	
-	public Jautajumi(String jautajumi, List<String> atbildes, char pareizAtb) {
-		this.jautajumi = jautajumi;
+	public Jautajumi(String jautajums, List<String> atbildes, char pareizAtb) {
+		this.jautajums = jautajums;
 		this.atbildes = atbildes;
 		this.pareizAtb = pareizAtb;
 	}
@@ -19,6 +22,29 @@ class Test{
 	int punkti =0;
 	int parezasatb = 0;
 	int nepareizasAtb = 0;
+	public void JautajumuIelade(String failanosaukums)throws IOException{
+		try(BufferedReader lasit = new BufferedReader(new FileReader(failanosaukums))){
+			String rinda;
+            while ((rinda = lasit.readLine()) != null) 
+			if(rinda.trim().isEmpty()) continue;
+			String jautajumi = rinda;
+			List<String> atbildes = new ArrayList<>();
+			for(int i = 0;1<4;i++) {
+				rinda = lasit.readLine();
+				if(rinda == null) {
+					throw new IOException("nepabeigts jautajums  failā");
+				}
+				atbildes.add(rinda);
+			}
+			rinda = lasit.readLine();
+			if(rinda == null) {
+				throw new IOException("zudusi jautajuma pareizā atbilde: "+ jautajumi);
+			}
+			char parezasatb = rinda.charAt(0);
+			jautajumi.add(new Jautajumi(jautajumi, atbildes, parezasatb));
+		
+		}
+	}
 }
 
 
